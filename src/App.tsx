caa5590248eb9c2056f68926050c59f3cd5de452
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2021-03-29 23:27:13 -0700

    basic init and adding credentials

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "clearblade-js-client/lib/mqttws31";
+import { ClearBlade } from "clearblade-js-client";
+import $ from "jquery";
+import { cbCredentials, collectionID } from "./credentials";
+
+const cb = new ClearBlade();
+
+cb.init({
+  URI: cbCredentials.URI, // e.g., 'https://platform.clearblade.com'
+  systemKey: cbCredentials.systemKey,
+  systemSecret: cbCredentials.systemSecret,
+  callback: initCallback,
+});
+
+function initCallback() {
+  var collection = cb.Collection(collectionID);
+  var query = cb.Query();
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/credentials.js
@@ -0,0 +1,7 @@
+export const cbCredentials = {
+  URI: "https://platform.clearblade.com", // e.g., 'https://platform.clearblade.com'
+  systemKey: "baa792860c9edc95fcacb4e2e076",
+  systemSecret: "BAA792860CA8BCEDFDCDE3DFD458",
+};
+
+export const collectionID = "94a593860cdebf87cf8387f3b4f001";
